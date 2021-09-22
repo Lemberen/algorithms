1d551fc07382aa25c75198d36867ab8404052458
Lemberen <gcfjab@gmail.com>
2021-09-21 21:37:52 -0300

    Added binary search

---
+++ binary_search/binary_search.ts
@@ -0,0 +1,27 @@
+/**
+ * Given a sorted array and a number, uses binary search to try and find that number in the array.
+ * 
+ * @param {number[]} sortedArray - A sorted array of numbers in ascending order.
+ * @param {number} userNumber - The number that is going to be searched in `sortedArray`.
+ * @returns {(number | null)} If `userNumber` is found, returns the position where it's located in the array. Otherwise, returns null.
+ */
+const binarySearch = (sortedArray: number[], userNumber: number): number | null => {
+    let low: number = 0;
+    let high: number = sortedArray.length - 1;
+    
+    if(sortedArray.length === 0)
+        return null;
+
+    while(low <= high) {
+        const mid: number = Math.floor((high + low) / 2);
+
+        if(sortedArray[mid] === userNumber)
+            return mid;
+        else if(sortedArray[mid] < userNumber)
+            low = mid + 1;
+        else
+            high = mid - 1;    
+    }
+
+    return null;
+};
